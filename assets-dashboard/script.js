2bb53a14f6672ae2fe5fb5ad377eee6a8ce707be
Vanessa Aurellia <vanessaaurellia01@gmail.com>
2022-07-20 19:40:25 +0700

    warna

--- assets-dashboard/script.js
+++ assets-dashboard/script.js
@@ -16,8 +16,8 @@ const data = {
     labels: labels,
     datasets: [{
       label: 'My First dataset',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgb(241, 135, 0)',
+      borderColor: 'rgb(241, 135, 0)',
       data: [0, 10, 5, 2, 20, 30, 45],
     }]
 };
