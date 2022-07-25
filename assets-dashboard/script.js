c34aaaa73d098117608fa694b6b4494c6a22424a
Vanessa Aurellia <vanessaaurellia01@gmail.com>
2022-07-25 15:54:29 +0700

    update

--- assets-dashboard/script.js
+++ assets-dashboard/script.js
@@ -2,6 +2,40 @@ function toggler(){
     document.getElementById('sidebar').classList.toggle('active');
 }
 
+// Replace with your view ID.
+var VIEW_ID = '<REPLACE_WITH_VIEW_ID>';
+
+// Query the API and print the results to the page.
+function queryReports() {
+  gapi.client.request({
+    path: '/v4/reports:batchGet',
+    root: 'https://analyticsreporting.googleapis.com/',
+    method: 'POST',
+    body: {
+      reportRequests: [
+        {
+          viewId: VIEW_ID,
+          dateRanges: [
+            {
+              startDate: '7daysAgo',
+              endDate: 'today'
+            }
+          ],
+          metrics: [
+            {
+              expression: 'ga:sessions'
+            }
+          ]
+        }
+      ]
+    }
+  }).then(displayResults, console.error.bind(console));
+}
+
+function displayResults(response) {
+  var formattedJson = JSON.stringify(response.result, null, 2);
+  document.getElementById('query-output').value = formattedJson;
+}
 
 const labels = [
     'January',
