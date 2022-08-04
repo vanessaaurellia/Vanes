29996a4ed8f6b50261f4c57e73074c0fe5cb7332
Vanessa Aurellia <vanessaaurellia01@gmail.com>
2022-08-04 13:37:13 +0700

    update

--- assets-dashboard/script.js
+++ assets-dashboard/script.js
@@ -13,53 +13,136 @@ function toggler(){
     }
 })*/
   
+const ctx = document.getElementById('myChart').getContext('2d');
+const ctx2 = document.getElementById('myChart2').getContext('2d');
+const ctx3 = document.getElementById('myChart3').getContext('2d');
+const ctx4 = document.getElementById('myChart4').getContext('2d');
+const ctx5 = document.getElementById('myChart6').getContext('2d');
 
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-];
-
-const data = {
-    labels: labels,
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgb(241, 135, 0)',
-      borderColor: 'rgb(241, 135, 0)',
-      data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-};
-
-const config = {
+const myChart = new Chart(ctx, {
     type: 'line',
-    data: data,
-    options: {}
-};
+    data: {
+        labels: [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+        ],
+        datasets: [{
+            label: 'Page view',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: 'rgb(241, 135, 0)',
+            borderColor: 'rgb(241, 135, 0)'
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
-const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-);
+const myChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+        ],
+        datasets: [{
+            label: 'Page view',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: 'rgb(241, 135, 0)',
+            borderColor: 'rgb(241, 135, 0)'
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
-const myChart2 = new Chart(
-    document.getElementById('myChart2'),
-    config
-);
+const myChart3 = new Chart(ctx3, {
+    type: 'line',
+    data: {
+        labels: [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+        ],
+        datasets: [{
+            label: 'Page view',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: 'rgb(241, 135, 0)',
+            borderColor: 'rgb(241, 135, 0)'
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
-const myChart3 = new Chart(
-    document.getElementById('myChart3'),
-    config
-);
+const myChart4 = new Chart(ctx4, {
+    type: 'line',
+    data: {
+        labels: [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+        ],
+        datasets: [{
+            label: 'Page view',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: 'rgb(241, 135, 0)',
+            borderColor: 'rgb(241, 135, 0)'
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
-const myChart4 = new Chart(
-    document.getElementById('myChart4'),
-    config
-);
-
-const myChart5 = new Chart(
-    document.getElementById('myChart5'),
-    config
-);
+const myChart5 = new Chart(ctx5, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'January',
+            'February',
+            'March',
+        ],
+        datasets: [{
+            data: [12, 19, 3],
+            backgroundColor: [
+                'rgba(118, 183, 67, 1)',
+                'rgba(249, 206, 48, 1)',
+                'rgba(224, 79, 26, 1)'
+            ],
+            hoverOffset: 4
+        }]
+    }
+});
